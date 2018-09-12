e66aacb25820426ddf851ff3fada56678d980914
Wizkiller96 <wizkiller96@hotmail.com>
2018-09-11 22:23:08 -0400

    Update youtube-dl before restarting. Added `youtube-dl -U`.

--- WizBotARU_Latest.sh
+++ WizBotARU_Latest.sh
@@ -5,7 +5,7 @@ root=$(pwd)
 youtube-dl -U
 
 sleep 5s
-while :; do cd "$root/WizBot" && dotnet restore && dotnet build -c Release && cd "$root/WizBot/src/WizBot" && dotnet run -c Release && cd "$root" && wget -N https://github.com/Wizkiller96/WizBot-BashScript/raw/1.9/wizbot_installer_latest.sh && bash "$root/wizbot_installer_latest.sh"; sleep 5s; done
+while :; do cd "$root/WizBot" && dotnet restore && dotnet build -c Release && cd "$root/WizBot/src/WizBot" && dotnet run -c Release && youtube-dl -U && cd "$root" && wget -N https://github.com/Wizkiller96/WizBot-BashScript/raw/1.9/wizbot_installer_latest.sh && bash "$root/wizbot_installer_latest.sh"; sleep 5s; done 
 echo ""
 echo "That didn't work? Please report in WizNet Discord Server."
 sleep 3s
